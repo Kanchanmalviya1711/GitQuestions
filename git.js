12cf92e9f4672c25a7d9e512ed3734ed2c01fbea
kanchanMalviya <kanchan@newrise.in>
2023-07-27 12:04:26 +0530

    first commit

---
+++ git.js
@@ -0,0 +1,39 @@
+// What is Git, and what are its advantages over other version control systems?
+
+// Explain the basic Git workflow.
+
+// How do you create a new Git repository?
+
+// What are branches in Git, and why are they useful?
+
+// How do you switch between branches in Git?
+
+// What is a "merge" in Git, and how does it work?
+
+// How do you resolve merge conflicts in Git?
+
+// What is the purpose of the "git clone" command, and how is it different from "git pull"?
+
+// How do you revert a commit in Git?
+
+// Explain the difference between "git fetch" and "git pull."
+
+// What is the purpose of the ".gitignore" file, and how is it used?
+
+// How do you create a new branch in Git?
+
+// What is a "pull request" in GitHub, and how does it facilitate collaboration?
+
+// How do you squash multiple commits into a single commit in Git?
+
+// How do you undo the last commit without losing the changes in Git?
+
+// Explain the concept of "rebasing" in Git and when it's used.
+
+// How do you tag a specific commit in Git?
+
+// What is the difference between "git rebase" and "git merge"?
+
+// How do you create a new repository on GitHub and push your local code to it?
+
+// What are GitHub Actions, and how can they be used to automate workflows?
